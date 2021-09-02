a34afbf93438782cba0ff5065b5163518f33e71c
mathias <math260d@zbc.dk>
2021-09-02 21:14:26 +0200

    commit on master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllertComponent } from './Components/allert/allert.component';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap' ;
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
+
 })
 export class AppModule { }
